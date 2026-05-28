949b37f1197ce2a4049a44eb57eaaee5ed2be371
dushechka <suskaman@mail.ru>
2026-05-28 22:41:35 +0200

    done tasks 1-4 with extra

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -7,7 +7,13 @@ def test_init_product(samsung_product):
 
 def test_init_category(samsung_category, samsung_product):
     assert samsung_category.name == "Смартфоны"
-    assert samsung_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    assert samsung_category.products == [samsung_product]
+    assert (
+        samsung_category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
+    assert (
+        samsung_category.products
+        == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    )
     assert samsung_category.product_count == 1
     assert samsung_category.category_count == 1
