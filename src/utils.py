2e6f498aa8ab5e3c8e1a449741ce6997972af164
dushechka <suskaman@mail.ru>
2026-06-08 21:11:35 +0200

    fix utils.py and test_utiks.py

--- src/utils.py
+++ src/utils.py
@@ -7,86 +7,63 @@ from src.catalog import Category, Product
 utils_logger = logging.getLogger("utils")
 
 
-def get_product_from_json(file_name: str) -> list:
-    """get list of products from json file and create a class object"""
+def get_data_from_json(file_name: str) -> tuple:
+    """get lists of categories and products from json file and create a class object"""
 
     utils_logger.info("START getting list of products from json file")
 
     try:
         if not isinstance(file_name, str):
             raise TypeError("file_name must be a string")
+        if file_name == "":
+            raise ValueError("file_name must be a string")
 
         with open(file_name, "r", encoding="utf-8") as json_file:
             utils_logger.info(f"open file: {file_name}")
             data = json.load(json_file)
 
-        list_of_products = []
-
+        list_of_product = []
+        list_of_category = []
         for category in data:
+            cat = Category(
+                category["name"],
+                category["description"],
+                []
+            )
+
             for product in category["products"]:
-                list_of_products.append(
-                    Product(
-                        product["name"],
-                        product["description"],
-                        product["price"],
-                        product["quantity"],
-                    )
+                prod = Product(
+                                product["name"],
+                                product["description"],
+                                product["price"],
+                                product["quantity"],
+                                cat
                 )
 
-        return list_of_products
+                list_of_product.append(prod)
+                cat.add_product(prod)
+
+            list_of_category.append(cat)
+
+        return list_of_category, list_of_product
 
     except TypeError as e:
         utils_logger.error(f"ERROR {e}")
-        return []
+        return ()
+    except ValueError as e:
+        utils_logger.error(f"ERROR {e}")
+        return ()
 
     finally:
         utils_logger.info("START getting list of products from json file")
 
 
-def get_category_from_json(file_name: str) -> list:
-    """get list of categories from json file and create a class object"""
-
-    utils_logger.info("START getting list of categories from json file")
-
-    try:
-
-        if not isinstance(file_name, str):
-            raise TypeError("file_name must be a string")
-
-        list_of_product = get_product_from_json(file_name)
-
-        with open(file_name, "r", encoding="utf-8") as json_file:
-            utils_logger.info(f"open file: {file_name}")
-            data = json.load(json_file)
-
-        list_of_category = []
-
-        for category in data:
-            for product in list_of_product:
-                if product.category:
-                    list_of_category.append(
-                        Category(
-                            category["name"],
-                            category["description"],
-                            category["products"],
-                        )
-                    )
-
-        return list_of_category
-
-    except TypeError as e:
-        utils_logger.error(f"ERROR {e}")
-        return []
-
-    finally:
-        utils_logger.info("END getting list of categories from json file")
-
-
 if __name__ == "__main__":
     setup_logging()
 
-    cat = get_category_from_json(
+    categories, products = get_product_from_json(
         "C:/Users/suska/PycharmProjects/E_commerce_project/data/products.json"
     )
 
-    print(cat)
+    print(categories[1].name)
+    print(products)
